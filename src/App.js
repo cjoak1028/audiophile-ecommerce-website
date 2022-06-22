43bd6b604cd6f0cc7859d57fac9ae032eaa7ab50
David Kim <cjoak1028@gmail.com>
2022-06-22 12:24:37 -0400

    added home component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 
 const App = () => {
   return (
     <React.Fragment>
       <Navbar />
-      {/* <h1>HEADING-1</h1>
-      <h2>HEADING-2</h2>
-      <h3>HEADING-3</h3>
-      <h4>HEADING-4</h4>
-      <h5>HEADING-5</h5>
-      <h6>HEADING-6</h6>
-      <p className="overline">OVERLINE</p>
-      <p className="sub-title">SUB-TITLE</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> */}
+      <Home />
     </React.Fragment>
   )
 }
