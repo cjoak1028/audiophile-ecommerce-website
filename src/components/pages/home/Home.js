0e2442f41db9b8960919e17d2ca0929101f4b1b7
David Kim <cjoak1028@gmail.com>
2022-06-22 14:50:37 -0400

    created btn class and btn--orange class

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -9,6 +9,7 @@ const Home = () => {
                     <h6>New Product</h6>
                     <h1>XX99 Mark II Headphones</h1>
                     <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+                    <a className={'btn btn--orange'}>See Product</a>
                 </div>
             </div>
         </div>
