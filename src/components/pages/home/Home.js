30f875e2066cce05c4bc63801e98b9b7a91bfd4d
David Kim <cjoak1028@gmail.com>
2022-06-22 14:23:50 -0400

    added banner without button

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
-import HamburgerMenu from '../../shared/HamburgerMenu';
+import classes from './Home.module.scss';
 
 const Home = () => {
     return (
-        <React.Fragment>
-            <HamburgerMenu />
-        </React.Fragment>
+        <div className={classes.home}>
+            <div className={classes.banner}>
+                <div className={classes["banner__content"]}>
+                    <h6>New Product</h6>
+                    <h1>XX99 Mark II Headphones</h1>
+                    <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
