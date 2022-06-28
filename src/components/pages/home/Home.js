8179c8d551eb805080183562f6903b75b5194c20
David Kim <cjoak1028@gmail.com>
2022-06-28 14:52:05 -0400

    created featured-products for mobile

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -4,6 +4,7 @@ import CategoryLinks from '../../shared/CategoryLinks/CategoryLinks';
 import classes from './Home.module.scss';
 
 import speakerZx9Img from '../../../assets/home/mobile/image-speaker-zx9.png';
+import earphonesYx1Img from '../../../assets/home/mobile/image-earphones-yx1.jpg';
 
 const Home = () => {
     return (
@@ -16,23 +17,26 @@ const Home = () => {
                     <a className={'btn btn--orange'}>See Product</a>
                 </div>
             </div>
-            <div className={`content-wrapper padding-x ${classes["home__content"]}`}>
+            <div className={`content-wrapper ${classes["home__content"]}`}>
                 <CategoryLinks />
-                <div className={classes['home-gallery']}>
-                    <div className={classes['home-gallery__cell-1']}>
-                        <div>
-                            <img src={speakerZx9Img}></img>
-                            <h1>ZX9 Speaker</h1>
-                            <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
-                            <a className={'btn btn--black'}>See Product</a>
-                        </div>
+                <div className={classes['featured-products']}>
+                    <div className={`${classes['featured-products__cell']} ${classes['featured-products__cell--1']}`}>
+                        <img src={speakerZx9Img} />
+                        <h1>ZX9 Speaker</h1>
+                        <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
+                        <a className={'btn btn--black'}>See Product</a>
                     </div>
-                    <div className={classes['home-gallery__cell-2']}>
+                    <div className={`${classes['featured-products__cell']} ${classes['featured-products__cell--2']}`}>
                         <h4>ZX7 Speaker</h4>
                         <a className={'btn btn--transparent'}>See Product</a>
                     </div>
-                    <div className={classes['home-gallery__cell-3']}>3</div>
-                    <div className={classes['home-gallery__cell-4']}>4</div>
+                    <div className={`${classes['featured-products__cell']} ${classes['featured-products__cell--3']}`}>
+                        <img src={earphonesYx1Img} />
+                    </div>
+                    <div className={`${classes['featured-products__cell']} ${classes['featured-products__cell--4']}`}>
+                        <h4>YX1 Earphones</h4>
+                        <a className={'btn btn--transparent'}>See Product</a>
+                    </div>
                 </div>
             </div>
         </div >
