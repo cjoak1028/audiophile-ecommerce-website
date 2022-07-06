21d3553c1397af9e527955914da0ce7684117677
David Kim <cjoak1028@gmail.com>
2022-07-06 14:09:03 -0400

    refactored footer so that it sticks to the bottom of page

--- src/pages/HeadphonesPage/HeadphonesPage.js
+++ src/pages/HeadphonesPage/HeadphonesPage.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 import classes from './HeadphonesPage.module.scss';
 
+import CategoryLinks from '../../components/CategoryLinks';
+import About from '../../components/About';
+
 const HeadphonesPage = () => {
     return (
         <div className={`${classes['headphones-page']}`}>
             <div className='flex flex-jc-c flex-ai-c banner banner--category'>
                 <h2>Headphones</h2>
             </div>
+            {/* <div className='padding-x content-wrapper'>
+                <CategoryLinks />
+                <About />
+            </div> */}
         </div>
     );
 }
