cecf328f5aa87672ca1847828c3dfa2868e560f4
David Kim <cjoak1028@gmail.com>
2022-07-09 14:48:39 -0400

    implemented product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import classes from './Product.module.scss';
+
 const Product = (props) => {
     return (
-        <p>Product</p>
+        <div className={classes['product']}>
+            <img className={classes['product__img']} src={props.imgSrc} alt={`${props.model} preview`} />
+            <div className={classes['product__text']}>
+                <h2 className={classes['product__model']}>{props.model}</h2>
+                <p className={classes['product__description']}>{props.description}</p>
+                <a className={'btn btn--orange'}>See Product</a>
+            </div>
+        </div>
     );
 };
 
--- src/pages/HeadphonesPage/HeadphonesPage.js
+++ src/pages/HeadphonesPage/HeadphonesPage.js
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react';
 
 import classes from './HeadphonesPage.module.scss';
 
+import Product from '../../components/Product';
 import CategoryLinks from '../../components/CategoryLinks';
 import About from '../../components/About';
+
+import image from '../../assets/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg';
 
 const HeadphonesPage = () => {
     const [data, setData] = useState(null);
@@ -25,7 +28,13 @@ const HeadphonesPage = () => {
 
     useEffect(() => {
         getData();
-    }, [])
+    }, []);
+
+    const headphonesProductList = data ? data[0].products.map(product => {
+        return (
+            <Product imgSrc={image} model={product.model} description={product.description} />
+        )
+    }) : '';
 
     return (
         <div className={`${classes['headphones-page']}`}>
@@ -33,6 +42,9 @@ const HeadphonesPage = () => {
                 <h2>Headphones</h2>
             </div>
             <div className='padding-x content-wrapper'>
+                <div className={classes['headphones-page__products']}>
+                    {headphonesProductList}
+                </div>
                 <CategoryLinks />
                 <About />
             </div>
